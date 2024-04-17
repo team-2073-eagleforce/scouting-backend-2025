e8d65e735165c5e2f3353f68b4b4509e4eb61f0a
caydenGranado <caydeng900@gmail.com>
2024-04-17 11:38:41 -0700

    hard coded comp code for milstein

--- scanner/views.py
+++ scanner/views.py
@@ -17,7 +17,8 @@ def scanner(request):
         Teams.objects.get_or_create(team_number=int(data_from_post["teamNumber"]), event=data_from_post["comp_code"])
         # Creates a new Team_Match_Data with given data if it doesn't exist
         Team_Match_Data.objects.get_or_create(team_number=int(data_from_post["teamNumber"]),
-                                              event=data_from_post["comp_code"],
+                                            #   event=data_from_post["comp_code"],
+                                              event="2024mil",
                                               match_number=data_from_post["matchNumber"],
                                               quantifier=data_from_post["quantifier"],
 
