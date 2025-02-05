2495490654dbe15081a0a3272272858546255de9
list three <cluckdukfuo1245@gmail.com>
2025-02-05 02:14:59 +0000

    pls work

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install -r requirements.txt
+# Use python3 -m pip instead of just pip
+python3 -m pip install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
