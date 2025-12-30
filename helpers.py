b047189e90a680c627c931d43f1662fae52b1aa3
Christopher Luk <chrisccluk29@gmail.com>
2025-12-30 12:34:42 -0800

    remove debug print statement from login_required wrapper

--- helpers.py
+++ helpers.py
@@ -3,7 +3,6 @@ from django.http import HttpResponseRedirect
 
 def login_required(function):
     def wrapper(request, *args, **kw):
-        print(request.session.get("email"))
         if not request.session.get("email"):
             return HttpResponseRedirect('/auth/')
         else:
